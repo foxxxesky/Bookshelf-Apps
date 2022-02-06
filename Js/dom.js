6f70e7f548b09f43ed92fadbbf2b7773eac1d867
foxxxesky <abdurrahman1467@gmail.com>
2022-02-06 19:34:05 +0700

    display uncompleted reading books

---
+++ Js/dom.js
@@ -0,0 +1,36 @@
+const UNCOMPLETED_READING_BOOKS = 'uncompleted';
+
+function readingBooks(bookTitle, penulisBuku, tahunTerbit) {
+  // Judul
+  const textTitle = document.createElement('h5');
+  textTitle.innerText = bookTitle;
+
+  //   Penulis
+  const textPenulis = document.createElement('p');
+  textPenulis.innerText = penulisBuku;
+
+  // Tahun
+  const textTahun = document.createElement('p');
+  textTahun.innerText = tahunTerbit;
+
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('pt-3');
+  textContainer.append(textTitle, textPenulis, textTahun);
+
+  return textContainer;
+}
+
+function addBooks() {
+  const uncompletedReadingBooks = document.getElementById(UNCOMPLETED_READING_BOOKS);
+
+  const bookTitle = document.getElementById('judul_buku').value;
+  const penulisBuku = document.getElementById('penulis').value;
+  const tahunTerbit = document.getElementById('tahun').value;
+
+  console.log('judulBuku ' + bookTitle);
+  console.log('penulis ' + penulisBuku);
+  console.log('tahun ' + tahunTerbit);
+
+  const books = readingBooks(bookTitle, penulisBuku, tahunTerbit);
+  uncompletedReadingBooks.append(books);
+}
